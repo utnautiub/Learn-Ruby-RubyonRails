52f55920ff57a3b9bbf78116a8df4428eb62522e
utnautiub <utnautiub@gmail.com>
2024-12-09 18:28:34 +0700

    update

---
+++ Ruby/index.rb
@@ -0,0 +1,24 @@
+# Cu phap co ban
+
+age = 30
+pi = 3.14
+name = :BuiTuanTu
+is_active = true
+address = nil
+
+# puts age, name, is_active, pi, address
+
+# Example Symbols vs String
+
+# String
+str = "Bui Tuan Tu"
+puts str[0]
+str[0] = '123'
+puts str
+
+# Symbols
+sys = :"Bui Tuan Tu"
+puts sys[0]
+# sys[0] = '123'
+puts sys
+
